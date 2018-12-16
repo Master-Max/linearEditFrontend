296d02063b7d854423674ae7644fa2ea453d445a
mx@E <mglxp5@gmail.com>
2018-12-16 05:18:52 -0500

    New Canvas Component, PC timer works

--- src/App.js
+++ src/App.js
@@ -1,48 +1,92 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import CanvasComponent from './CanvasComponent.js'
 class App extends Component {
+  state = {
+    playerVideoSrc: "http://localhost:4001/video",
+    ready: false,
+    videoPlaying: false,
+    pcmTime: null
+  }
 
+  handleClick = (button) => {
+    console.log(button)
+    switch (button) {
+      case 'PC-PLAY':
+        this.setState({videoPlaying: true})
+        break;
+      case 'PC-STILL':
+        this.setState({videoPlaying: false})
+        break;
+      case 'PC-LOAD':
+        const newPlayerSource = prompt('Enter Video URL: ');
+        if (!!newPlayerSource && newPlayerSource != "") {
+          this.setState({playerVideoSrc: newPlayerSource})
+        }
+        break;
+      case 'PC-REWIND':
+        const pcm = document.getElementById("pc-monitor");
+
+        break;
+      case 'PC-FASTFORWARD':
+        break;
+      default:
+        console.log('Uh Oh...');
+    }
+  }
+
+  setPCMTime = (pcmTime) => {
+    this.setState({pcmTime})
+  }
+
+  formatTime = (time) => {
+    let fTime = new Date(null)
+    fTime.setSeconds(time)
+    return (fTime.toISOString().substr(11, 8))
+  }
 
   monitorRack = () => (
     <div id="monitors">
-      <canvas ref="player-monitor" width={640} height={360} />
+      <CanvasComponent width={640} height={360} src={this.state.playerVideoSrc} play={this.state.videoPlaying} setTime={this.setPCMTime}/>
       <canvas ref="recorder-monitor" width={640} height={360} />
     </div>
   )
+  //<video ref="video" src={this.state.playerVideoSrc} controls/>
+  //<video id="player-video" className="" src={this.state.playerVideoSrc} controls={true} />
+  //<canvas id="player-monitor" ref="player-monitor" width={640} height={360} />
 
   playerControls = () => (
     <div id="player">
       <p>PLAYER</p>
-      <div class="row right-justify">
-        <b class="light">IN</b>
-        <b class="light">OUT</b>
+      <div className="row right-justify">
+        <b className="light">IN</b>
+        <b className="light">OUT</b>
       </div>
-      <div class="clock">00:00:00</div>
-      <div class="center-div">
-        <div class="row">
-          <b class="switch blue-button">EJECT</b>
-          <b class="switch blue-button">LOAD</b>
+      <div className="clock">{this.formatTime(this.state.pcmTime)}</div>
+      <div className="center-div">
+        <div className="row">
+          <b onClick={() => this.handleClick('PC-LOAD')} className="switch blue-button">LOAD</b>
+          <b onClick={() => this.handleClick('PC-EJECT')} className="switch blue-button">EJECT</b>
         </div>
       </div>
       <br/>
-      <div class="center-div">
-        <div class="row">
-          <b class="switch">PLAY</b>
-          <b class="switch">STILL</b>
-          <b class="switch">REW</b>
-          <b class="switch">FF</b>
+      <div className="center-div">
+        <div className="row">
+          <b onClick={() => this.handleClick('PC-PLAY')} className="switch">PLAY</b>
+          <b onClick={() => this.handleClick('PC-STILL')} className="switch">STILL</b>
+          <b onClick={() => this.handleClick('PC-REWIND')} className="switch">REW</b>
+          <b onClick={() => this.handleClick('PC-FASTFORWARD')} className="switch">FF</b>
         </div>
       </div>
       <br/>
-      <div id="jogger" class="center-div">
-        <div class="center-div">
-          <b class="light">JOG</b>
+      <div id="jogger" className="center-div">
+        <div className="center-div">
+          <b className="light">JOG</b>
         </div>
-        <div class="row">
-          <b class="switch">{'<'}</b>
-          <b class="switch">{'>'}</b>
+        <div className="row">
+          <b onClick={() => this.handleClick('PC-DIAL-BACK')} className="switch">{'<'}</b>
+          <b onClick={() => this.handleClick('PC-DIAL-FORWARD')} className="switch">{'>'}</b>
         </div>
       </div>
     </div>
@@ -51,44 +95,44 @@ class App extends Component {
   editControls = () => (
     <div id="editor">
       <div id="main-edit">
-        <div class="part-box">
-          <div id="trim" class="center-div">
-            <div class="row"><div>
-              <div class="row">
-                <b class="switch">+</b>
-                <b class="switch">-</b>
+        <div className="part-box">
+          <div id="trim" className="center-div">
+            <div className="row"><div>
+              <div className="row">
+                <b onClick={() => this.handleClick('EC-TRIM-MINUS')} className="switch">-</b>
+                <b onClick={() => this.handleClick('EC-TRIM-PLUS')} className="switch">+</b>
               </div>
               <p>TRIM</p>
             </div></div>
           </div>
         </div>
-        <div id="timecode" class="part-box">
-          <div class="row">
+        <div id="timecode" className="part-box">
+          <div className="row">
             <div>
-              <div class="row">
-                <b class="switch">IN</b>
-                <b class="switch">OUT</b>
+              <div className="row">
+                <b onClick={() => this.handleClick('EC-PLAYER-IN')} className="switch">IN</b>
+                <b onClick={() => this.handleClick('EC-PLAYER-OUT')} className="switch">OUT</b>
               </div>
               <p>PLAYER</p>
             </div>
-            <b class="switch grey-button">GO TO</b>
+            <b onClick={() => this.handleClick('EC-GOTO')} className="switch grey-button">GO TO</b>
               <div>
-                <div class="row">
-                  <b class="switch">IN</b>
-                  <b class="switch">OUT</b>
+                <div className="row">
+                  <b onClick={() => this.handleClick('EC-RECORDER-IN')} className="switch">IN</b>
+                  <b onClick={() => this.handleClick('EC-RECORDER-OUT')} className="switch">OUT</b>
                 </div>
                 <p>RECORDER</p>
               </div>
           </div>
-        <div class="row">
-          <b class="switch switch-big grey-button">ENTRY</b>
+        <div className="row">
+          <b onClick={() => this.handleClick('EC-ENTRY')} className="switch switch-big grey-button">ENTRY</b>
         </div>
       </div>
       </div>
       <div id="render">
-        <div class="row">
-          <b class="switch switch-med">PREVIEW</b>
-          <b class="switch switch-med red-button">AUTO EDIT</b>
+        <div className="row">
+          <b onClick={() => this.handleClick('EC-PREVIEW')} className="switch switch-med">PREVIEW</b>
+          <b onClick={() => this.handleClick('EC-AUTO-EDIT')} className="switch switch-med red-button">AUTO EDIT</b>
         </div>
       </div>
     </div>
@@ -97,34 +141,34 @@ class App extends Component {
   recorderControls = () => (
     <div id="recorder">
       <p>RECORDER</p>
-      <div class="row right-justify">
-        <b class="light">IN</b>
-        <b class="light">OUT</b>
+      <div className="row right-justify">
+        <b className="light">IN</b>
+        <b className="light">OUT</b>
       </div>
-      <div class="clock">00:00:00</div>
-      <div class="center-div">
-        <div class="row">
-          <b class="switch blue-button">EJECT</b>
-          <b class="switch red-button">REC</b>
+      <div className="clock">00:00:00</div>
+      <div className="center-div">
+        <div className="row">
+          <b onClick={() => this.handleClick('RC-RECORD')} className="switch red-button">REC</b>
+          <b onClick={() => this.handleClick('RC-EXPORT')} className="switch blue-button">EXP</b>
         </div>
       </div>
       <br/>
-      <div class="center-div">
-        <div class="row">
-          <b class="switch">PLAY</b>
-          <b class="switch">STILL</b>
-          <b class="switch">REW</b>
-          <b class="switch">FF</b>
+      <div className="center-div">
+        <div className="row">
+          <b onClick={() => this.handleClick('RC-PLAY')} className="switch">PLAY</b>
+          <b onClick={() => this.handleClick('RC-STILL')} className="switch">STILL</b>
+          <b onClick={() => this.handleClick('RC-REWIND')} className="switch">REW</b>
+          <b onClick={() => this.handleClick('RC-FASTFORWARD')} className="switch">FF</b>
         </div>
       </div>
       <br/>
-      <div id="jogger" class="center-div">
-        <div class="center-div">
-          <b class="light">JOG</b>
+      <div id="jogger" className="center-div">
+        <div className="center-div">
+          <b className="light">JOG</b>
         </div>
-        <div class="row">
-          <b class="switch">{'<'}</b>
-          <b class="switch">{'>'}</b>
+        <div className="row">
+          <b onClick={() => this.handleClick('RC-DIAL-BACK')} className="switch">{'<'}</b>
+          <b onClick={() => this.handleClick('RC-DIAL-FORWARD')} className="switch">{'>'}</b>
         </div>
       </div>
     </div>
@@ -136,7 +180,7 @@ class App extends Component {
 
         {this.monitorRack()}
         <div id="controls">
-          <div class="flexy">
+          <div className="flexy">
             {this.playerControls()}
             {this.editControls()}
             {this.recorderControls()}
---
+++ src/CanvasComponent.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+function rect(props) {
+    const {ctx, x, y, width, height} = props;
+    ctx.fillRect(x, y, width, height);
+}
+
+class CanvasComponent extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      ctx: null,
+      v: null,
+    }
+  }
+
+  draw = (v,c,w,h) => {
+    // console.log('drawing')
+    if(v.paused || v.ended) return false;
+    // console.log(v)
+    // console.log(c)
+    c.drawImage(v,0,0,w,h);
+    // setTimeout();
+    this.props.setTime(v.currentTime)
+    setTimeout(this.draw,20,v,c,w,h);
+  }
+
+  setUp() {
+    console.log('setting up')
+
+
+
+
+    var v = this.v;
+    var canvas = this.refs.canvas;
+    var context = this.ctx;
+
+    // const v = this.state.v;
+    // const context = this.state.ctx;
+
+    // var cw = Math.floor(canvas.clientWidth / 100);
+    // var ch = Math.floor(canvas.clientHeight / 100);
+    // canvas.width = cw;
+    // canvas.height = ch;
+
+    v.addEventListener('play', () => {
+      console.log('playing');
+      this.draw(v,context,640,360);
+    },false);
+  }
+
+
+  componentDidMount() {
+    // const ctx = this.refs.canvas.getContext('2d');
+    // const v = this.refs.video
+    this.ctx = this.refs.canvas.getContext('2d');
+    this.v = this.refs.video
+    this.setUp();
+    // this.setState({ctx, v}, this.setUp())
+    // this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    if(this.props.play){
+      this.v.play()
+    }else{
+      this.v.pause()
+    }
+    // const ctx = this.refs.canvas.getContext('2d');
+    // ctx.clearRect(0,0, this.props.width, this.props.height);
+    // // draw children “components”
+    // rect({ctx, x: 10, y: 10, width: 50, height: 50});
+    // rect({ctx, x: 110, y: 110, width: 50, height: 50});
+  }
+
+  render() {
+    return (
+      <>
+      <video id="pc-monitor" ref="video" className="hidden" src={this.props.src} controls/>
+      <canvas ref="canvas" width={this.props.width} height={this.props.height}/>
+      </>
+    );
+  }
+}
+
+export default CanvasComponent
+
+
+//<video ref="video" src={this.props.src} controls/>
